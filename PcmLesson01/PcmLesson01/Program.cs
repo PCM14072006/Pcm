b3efad937491813c8ed90e8e55055c5aea3313a4
PCM14072006 <pcm14072006@gmail.com>
2026-08-15 17:19:29 +0700

    Update project

---
+++ PcmLesson01/PcmLesson01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PcmLesson01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
